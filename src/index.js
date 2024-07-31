f2baca0b79b0c84157d43ef3aafacc49566c4c78
hcylus <hercylus@gmail.com>
2024-07-31 19:59:41 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.bitypes.com": dockerHub,
+  "quay.bitypes.com": "https://quay.io",
+  "gcr.bitypes.com": "https://gcr.io",
+  "k8s-gcr.bitypes.com": "https://k8s.gcr.io",
+  "k8s.bitypes.com": "https://registry.k8s.io",
+  "ghcr.bitypes.com": "https://ghcr.io",
+  "cloudsmith.bitypes.com": "https://docker.cloudsmith.io",
+  "ecr.bitypes.com": "https://public.ecr.aws",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.bitypes.com": dockerHub,
 };
 
 function routeByHosts(host) {
